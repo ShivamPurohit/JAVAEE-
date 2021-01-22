cf99612060282be83d7ab9840065626980a4eec2
Shivam Purohit <57129506+ShivamPurohit@users.noreply.github.com>
2021-01-22 07:20:11 +0530

    Add files via upload

---
+++ SpringBootDemo/firstproj/src/main/java/com/codesp07/first/Laptop.java
@@ -0,0 +1,10 @@
+package com.codesp07.first;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+	public void compile() {
+		System.out.println("Compiling..");
+	}
+}
---
+++ SpringBootDemo/firstproj/src/test/java/com/codesp07/first/FirstprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesp07.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
